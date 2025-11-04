7ef4a413db92526416d7103489c8c828d5c73ff0
thandiogunde <99139483+thandiogunde@users.noreply.github.com>
2025-11-04 13:48:40 +0000

    Add files via upload

---
+++ frontend/src/components/WordForm.vue
@@ -0,0 +1,68 @@
+<template>
+    <form action="#" @submit.prevent="onSubmit">
+       <p v-if="errorsPresent" class="error">Please fill out all fields!</p>
+   
+       <div class="ui labeled input fluid">
+         <div class="ui label">
+           <i class="germany flag"></i> German
+         </div>
+         <input type="text" placeholder="Enter word..." v-model="word.german" />
+       </div>
+   
+       <div class="ui labeled input fluid">
+         <div class="ui label">
+           <i class="united kingdom flag"></i> English
+         </div>
+         <input type="text" placeholder="Enter word..." v-model="word.english" />
+       </div>
+
+       <div class="ui labeled input fluid">
+         <div class="ui label">
+           <i class="italy flag"></i> Italian
+         </div>
+         <input type="text" placeholder="Enter word..." v-model="word.italian" />
+       </div>
+   
+       <button class="positive ui button">Submit</button>
+     </form>
+   </template>
+   
+   <script>
+   export default {
+     name: 'word-form',
+     props: {
+       word: {
+         type: Object,
+         required: false,
+         default: () => {
+            return {
+                english: '',
+                german: '',
+                italian: ''
+            };
+         }
+       }
+     },
+     
+     data() {
+       return {
+         errorsPresent: false
+       };
+     },
+     methods: {
+       onSubmit: function() {
+        if (this.word.english === '' || this.word.german === '' || this.word.italian === '') {
+            this.errorsPresent = true;
+        } else {
+            this.$emit('createOrUpdate', this.word);
+        }
+       }
+     }
+   };
+   </script>
+   
+   <style scoped>
+   .error {
+     color: red;
+   }
+   </style>
---
+++ frontend/src/views/TO05.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Welcome to Thandi's Page &#128512</h1>
+      <p>This page was added to demonstrate my ability to add a new item to the menu bar.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TO05',
+    
+  };
+  </script>
